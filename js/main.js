595a72da3b1bfaa5ac45ad1975f5de26c1ee75ca
iTanvir <thisistanvir01@gmail.com>
2021-01-12 19:57:06 +0600

    Add Style Swithcher and light/dark Mode

--- js/main.js
+++ js/main.js
@@ -1,4 +1,73 @@
 
+/************************** Navigation Menu ****************************/ 
+(()=>{
+   const hamburgerBtn = document.querySelector('.hamburger-btn'),
+         navMenu = document.querySelector('.nav-menu'),
+         closeNavBtn = navMenu.querySelector('.close-nav-menu');
+
+         hamburgerBtn.addEventListener('click', showNavMenu);
+         closeNavBtn.addEventListener('click', hideNavMenu);
+
+   function showNavMenu(){
+      navMenu.classList.add('open')
+      bodyScrollingToggle()
+   }
+   function hideNavMenu(){
+      navMenu.classList.remove('open')
+      fadeOutEffect();
+      bodyScrollingToggle()
+   }
+   function fadeOutEffect(){
+      document.querySelector('.fade-out-effect').classList.add('active');
+      setTimeout(()=>{
+         document.querySelector('.fade-out-effect').classList.remove('active');
+      },300);
+   }
+
+   // attach an     event handler to document
+   document.addEventListener('click', (event)=>{
+      if(event.target.classList.contains('link-item')){
+         // make sure event.target.hash has a value before overriding default behavior
+         if(event.target.hash !== ""){
+            // prevent default anchor click behavior
+            event.preventDefault();
+            const hash = event.target.hash;
+            // deactivate existing active 'section'
+            document.querySelector('.section.active').classList.add('hide')
+            document.querySelector('.section.active').classList.remove('active')
+            // activate new 'section'
+            document.querySelector(hash).classList.add('active')
+            document.querySelector(hash).classList.remove('hide')
+            // deactivate existing active navigation menu 'link-item'
+            navMenu.querySelector('.active').classList.add('outer-shadow', 'hover-in-shadow')
+            navMenu.querySelector('.active').classList.remove('active', 'inner-shadow')
+            // if clicked 'link-item is contained within the navigation menu
+            if(navMenu.classList.contains('open')){
+               //activate new navigation menu 'link-item'
+               event.target.classList.add('active', 'inner-shadow')
+               event.target.classList.remove('outer-shadow', 'hover-in-shadow');
+               // hide navigation menu
+               hideNavMenu();
+            }else{
+               let navItems = navMenu.querySelectorAll('.link-item')
+               navItems.forEach((item)=>{
+                  if(hash === item.hash){
+                     //activate new navigation menu 'link-item'
+                     item.classList.add('active', 'inner-shadow')
+                     item.classList.remove('outer-shadow', 'hover-in-shadow');
+
+                  }
+               })
+               fadeOutEffect()
+            }
+            // add hash (#) to url
+            window.location.hash = hash;
+         }
+      }
+   })
+
+})();
+
 
 /**************************** About Section **************************************/
 (function(){
@@ -224,4 +293,17 @@ function bodyScrollingToggle(){
             sliderContainer.style.marginLeft = - (sliderWidth * slideIndex) + 'px';
          }
          slider();
+})();
+
+
+
+
+/************************** Hide All Section Except Active ****************************/ 
+(()=>{
+   const sections = document.querySelectorAll('.section');
+   sections.forEach((section)=>{
+      if(!section.classList.contains('active')){
+         section.classList.add('hide');
+      }
+   })
 })();
