963413bdc498a88295b1da35bab87bdae8855c78
iTanvir <thisistanvir01@gmail.com>
2021-01-12 17:18:36 +0600

    Design Cantact Area

--- js/main.js
+++ js/main.js
@@ -175,3 +175,53 @@ function bodyScrollingToggle(){
          }
 
 })();
+
+
+
+
+/************************** Testimonial ****************************/ 
+(()=>{
+
+   const sliderContainer = document.querySelector('.testi-slider-container'),
+         slides = sliderContainer.querySelectorAll('.testi-item'),
+         sliderWidth =sliderContainer.offsetWidth,
+         prevBtn = document.querySelector('.testi-slider-nav .prev'),
+         nextBtn = document.querySelector('.testi-slider-nav .next'),
+         activeSlide = sliderContainer.querySelector('.testi-item.active');
+         let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(activeSlide);
+
+
+         // set width of all slides
+         slides.forEach((slide) =>{
+            slide.style.width = sliderWidth + 'px';
+         })
+         // set width of sliderContainer
+         sliderContainer.style.width = sliderWidth * slides.length + 'px';
+
+         nextBtn.addEventListener('click', ()=>{
+            if(slideIndex === slides.length-1){
+               slideIndex = 0;
+            }else{
+               slideIndex++;
+            }
+            slider()
+         })
+
+         prevBtn.addEventListener('click', ()=>{
+            if(slideIndex === 0){
+               slideIndex = slides.length-1;
+            }else{
+               slideIndex--;
+            }
+            slider()
+         })
+
+         function slider(){
+            //deactivate existing active slides
+            sliderContainer.querySelector('.testi-item.active').classList.remove('active')
+            // activate new slide
+            slides[slideIndex].classList.add('active');
+            sliderContainer.style.marginLeft = - (sliderWidth * slideIndex) + 'px';
+         }
+         slider();
+})();
